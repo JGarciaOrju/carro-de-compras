0761c2c929d16acf524a568ffed24b998df437c1
JGarciaOrju <julius1196@hotmail.com>
2025-09-14 20:28:38 -0500

    primer commit

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+const styles = {
+  title: {
+    marginBottom: '30px',
+  },
+};
+class Tittle extends Component {
+  render() {
+    return <h1 style={styles.title}>Tienda</h1>;
+  }
+}
+
+export default Tittle;
